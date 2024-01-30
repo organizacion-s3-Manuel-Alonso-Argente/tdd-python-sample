6cfd979330db2e6279a11278ee7c7544f456c6e4
Manuel Alonso Argente <145504941+manaloarg@users.noreply.github.com>
2024-01-30 07:37:31 +0100

    Create test_par.py

---
+++ tests/test_par.py
@@ -0,0 +1,13 @@
+# Librería pytest para ejecutar tests
+import pytest
+
+# Importar archivo de ejercicio
+from ejercicios.par import par
+
+# Clase para crear tests. Las funciones de testeo deberán crearse en esta clase
+class TestClass:
+
+    # Test para la operación suma
+    def test_par(self):
+        assert par(4) == "par"
+        assert par(5) == "impar"
