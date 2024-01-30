d093740e81fe3b06db08ebb5e189920df7c83e87
Manuel Alonso Argente <145504941+manaloarg@users.noreply.github.com>
2024-01-30 07:39:16 +0100

    Update par.py

--- ejercicios/par.py
+++ ejercicios/par.py
@@ -3,6 +3,6 @@ def par(x):
 
     # comprovación de si el número es par o impar
     if x % 2 == 0:
-      print("El número es par")
+      print("par")
     else:
-      print("El número es impar")
+      print("impar")
