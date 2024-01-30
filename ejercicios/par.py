af5a3984111adefaa2d4b1d4018a12e997f9f513
Manuel Alonso Argente <145504941+manaloarg@users.noreply.github.com>
2024-01-30 07:35:52 +0100

    Create par.py

---
+++ ejercicios/par.py
@@ -0,0 +1,8 @@
+# definición de la función par
+def par(x):
+
+    # comprovación de si el número es par o impar
+    if x % 2 == 0:
+      print("El número es par")
+    else:
+      print("El número es impar")
